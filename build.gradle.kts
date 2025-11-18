493bfa1465e6c0899fceddb8f51be5bba981d9f4
Ivan Vasilev <112619137+ivvas1@users.noreply.github.com>
2025-11-18 14:32:45 +0300

    Add initial build configuration with dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+    application
+}
+group = "ru.spbu.math-cs"
+version = "1.0-SNAPSHOT"
+repositories {
+    mavenCentral()
+    maven("https://packages.jetbrains.team/maven/p/skija/maven")
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+val osName = System.getProperty("os.name")
+val targetOs = when {
+    osName == "Mac OS X" -> "macos"
+    osName.startsWith("Win") -> "windows"
+    osName.startsWith("Linux") -> "linux"
+    else -> error("Unsupported OS: $osName")
+}
+val osArch = System.getProperty("os.arch")
+var targetArch = when (osArch) {
+    "x86_64", "amd64" -> "x64"
+    "aarch64" -> "arm64"
+    else -> error("Unsupported arch: $osArch")
+}
+val versionk = "0.7.34"
+val target = "${targetOs}-${targetArch}"
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.skija:skija-windows:0.93.6")
+    implementation("org.jetbrains.skiko:skiko-awt-runtime-$target:$versionk")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing")
+}
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+application {
+    mainClass.set("MainKt")
+}
